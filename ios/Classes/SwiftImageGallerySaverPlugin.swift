b6199b9a3f7e31e42d228fd97af9df68cc0d7890
xiaolouxinyue <xiaolouxinyue@outlook.com>
2023-03-23 18:20:51 +0800

    1.  support latitude, longitude, createDate on iOS

--- ios/Classes/SwiftImageGallerySaverPlugin.swift
+++ ios/Classes/SwiftImageGallerySaverPlugin.swift
@@ -3,8 +3,6 @@ import UIKit
 import Photos
 
 public class SwiftImageGallerySaverPlugin: NSObject, FlutterPlugin {
-    let errorMessage = "保存失败,请检查权限是否开启"
-    
     var result: FlutterResult?;
 
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -21,143 +19,151 @@ public class SwiftImageGallerySaverPlugin: NSObject, FlutterPlugin {
             let image = UIImage(data: imageData),
             let quality = arguments["quality"] as? Int,
             let _ = arguments["name"],
-            let isReturnImagePath = arguments["isReturnImagePathOfIOS"] as? Bool
-            else { return }
-        let newImage = image.jpegData(compressionQuality: CGFloat(quality / 100))!
-        saveImage(UIImage(data: newImage) ?? image, isReturnImagePath: isReturnImagePath)
+            let isReturnImagePath = arguments["isReturnImagePathOfIOS"] as? Bool else { return }
+          let latitude = arguments["latitude"] as? Double
+          let longitude = arguments["longitude"] as? Double
+          let createDate = arguments["createDate"] as? Int
+          let newImage = image.jpegData(compressionQuality: CGFloat(quality / 100))!
+          saveImage(UIImage(data: newImage) ?? image, isReturnImagePath: isReturnImagePath, latitude: latitude, longitude: longitude, createDate: createDate)
       } else if (call.method == "saveFileToGallery") {
         guard let arguments = call.arguments as? [String: Any],
               let path = arguments["file"] as? String,
               let _ = arguments["name"],
               let isReturnFilePath = arguments["isReturnPathOfIOS"] as? Bool else { return }
-        if (isImageFile(filename: path)) {
-            saveImageAtFileUrl(path, isReturnImagePath: isReturnFilePath)
+          let latitude = arguments["latitude"] as? Double
+          let longitude = arguments["longitude"] as? Double
+          let createDate = arguments["createDate"] as? Int
+        if isImageFile(filename: path) {
+            saveImageAtFileUrl(path, isReturnImagePath: isReturnFilePath, latitude: latitude, longitude: longitude, createDate: createDate)
         } else {
-            if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path)) {
-                saveVideo(path, isReturnImagePath: isReturnFilePath)
-            }
+            saveVideo(path, isReturnImagePath: isReturnFilePath, latitude: latitude, longitude: longitude, createDate: createDate)
         }
       } else {
         result(FlutterMethodNotImplemented)
       }
     }
     
-    func saveVideo(_ path: String, isReturnImagePath: Bool) {
-        if !isReturnImagePath {
-            UISaveVideoAtPathToSavedPhotosAlbum(path, self, #selector(didFinishSavingVideo(videoPath:error:contextInfo:)), nil)
-            return
-        }
+    func saveVideo(_ path: String, isReturnImagePath: Bool, latitude: Double? = nil, longitude: Double? = nil, createDate: Int? = nil) {
         var videoIds: [String] = []
         
         PHPhotoLibrary.shared().performChanges( {
             let req = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL.init(fileURLWithPath: path))
+            if let latitude = latitude, let longitude = longitude {
+                req?.location = CLLocation(latitude: latitude, longitude: longitude)
+            }
+            if let timeInterval = createDate {
+                req?.creationDate = Date(timeIntervalSince1970: Double(timeInterval))
+            }
             if let videoId = req?.placeholderForCreatedAsset?.localIdentifier {
                 videoIds.append(videoId)
             }
         }, completionHandler: { [unowned self] (success, error) in
             DispatchQueue.main.async {
-                if (success && videoIds.count > 0) {
+                if success && videoIds.count > 0 {
                     let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: videoIds, options: nil)
-                    if (assetResult.count > 0) {
-                        let videoAsset = assetResult[0]
-                        PHImageManager().requestAVAsset(forVideo: videoAsset, options: nil) { (avurlAsset, audioMix, info) in
-                            if let urlStr = (avurlAsset as? AVURLAsset)?.url.absoluteString {
-                                self.saveResult(isSuccess: true, filePath: urlStr)
+                    if assetResult.count > 0 {
+                        if isReturnImagePath {
+                            let videoAsset = assetResult[0]
+                            PHImageManager().requestAVAsset(forVideo: videoAsset, options: nil) { (avurlAsset, audioMix, info) in
+                                if let urlStr = (avurlAsset as? AVURLAsset)?.url.absoluteString {
+                                    self.saveResult(isSuccess: true, filePath: urlStr)
+                                }
                             }
+                        } else {
+                            self.saveResult(isSuccess: true, filePath: nil)
                         }
                     }
                 } else {
-                    self.saveResult(isSuccess: false, error: self.errorMessage)
+                    self.saveResult(isSuccess: false, error: error)
                 }
             }
         })
     }
     
-    func saveImage(_ image: UIImage, isReturnImagePath: Bool) {
-        if !isReturnImagePath {
-            UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
-            return
-        }
-        
+    func saveImage(_ image: UIImage, isReturnImagePath: Bool, latitude: Double? = nil, longitude: Double? = nil, createDate: Int? = nil) {
         var imageIds: [String] = []
         
         PHPhotoLibrary.shared().performChanges( {
             let req = PHAssetChangeRequest.creationRequestForAsset(from: image)
+            if let latitude = latitude, let longitude = longitude {
+                req.location = CLLocation(latitude: latitude, longitude: longitude)
+            }
+            if let timeInterval = createDate {
+                req.creationDate = Date(timeIntervalSince1970: Double(timeInterval))
+            }
             if let imageId = req.placeholderForCreatedAsset?.localIdentifier {
                 imageIds.append(imageId)
             }
         }, completionHandler: { [unowned self] (success, error) in
             DispatchQueue.main.async {
-                if (success && imageIds.count > 0) {
+                if success && imageIds.count > 0 {
                     let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: imageIds, options: nil)
-                    if (assetResult.count > 0) {
-                        let imageAsset = assetResult[0]
-                        let options = PHContentEditingInputRequestOptions()
-                        options.canHandleAdjustmentData = { (adjustmeta)
-                            -> Bool in true }
-                        imageAsset.requestContentEditingInput(with: options) { [unowned self] (contentEditingInput, info) in
-                            if let urlStr = contentEditingInput?.fullSizeImageURL?.absoluteString {
-                                self.saveResult(isSuccess: true, filePath: urlStr)
+                    if assetResult.count > 0 {
+                        if isReturnImagePath {
+                            let imageAsset = assetResult[0]
+                            let options = PHContentEditingInputRequestOptions()
+                            options.canHandleAdjustmentData = { (adjustmeta)
+                                -> Bool in true }
+                            imageAsset.requestContentEditingInput(with: options) { [unowned self] (contentEditingInput, info) in
+                                if let urlStr = contentEditingInput?.fullSizeImageURL?.absoluteString {
+                                    self.saveResult(isSuccess: true, filePath: urlStr)
+                                }
                             }
+                        } else {
+                            self.saveResult(isSuccess: true, filePath: nil)
                         }
                     }
                 } else {
-                    self.saveResult(isSuccess: false, error: self.errorMessage)
+                    self.saveResult(isSuccess: false, error: error)
                 }
             }
         })
     }
     
-    func saveImageAtFileUrl(_ url: String, isReturnImagePath: Bool) {
-        if !isReturnImagePath {
-            if let image = UIImage(contentsOfFile: url) {
-                UIImageWriteToSavedPhotosAlbum(image, self, #selector(didFinishSavingImage(image:error:contextInfo:)), nil)
-            }
-            return
-        }
-        
+    func saveImageAtFileUrl(_ url: String, isReturnImagePath: Bool, latitude: Double? = nil, longitude: Double? = nil, createDate: Int? = nil) {
         var imageIds: [String] = []
         
         PHPhotoLibrary.shared().performChanges( {
             let req = PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: URL(string: url)!)
+            if let latitude = latitude, let longitude = longitude {
+                req?.location = CLLocation(latitude: latitude, longitude: longitude)
+            }
+            if let timeInterval = createDate {
+                req?.creationDate = Date(timeIntervalSince1970: Double(timeInterval))
+            }
             if let imageId = req?.placeholderForCreatedAsset?.localIdentifier {
                 imageIds.append(imageId)
             }
         }, completionHandler: { [unowned self] (success, error) in
             DispatchQueue.main.async {
-                if (success && imageIds.count > 0) {
+                if success && imageIds.count > 0 {
                     let assetResult = PHAsset.fetchAssets(withLocalIdentifiers: imageIds, options: nil)
-                    if (assetResult.count > 0) {
-                        let imageAsset = assetResult[0]
-                        let options = PHContentEditingInputRequestOptions()
-                        options.canHandleAdjustmentData = { (adjustmeta)
-                            -> Bool in true }
-                        imageAsset.requestContentEditingInput(with: options) { [unowned self] (contentEditingInput, info) in
-                            if let urlStr = contentEditingInput?.fullSizeImageURL?.absoluteString {
-                                self.saveResult(isSuccess: true, filePath: urlStr)
+                    if assetResult.count > 0 {
+                        if isReturnImagePath {
+                            let imageAsset = assetResult[0]
+                            let options = PHContentEditingInputRequestOptions()
+                            options.canHandleAdjustmentData = { (adjustmeta)
+                                -> Bool in true }
+                            imageAsset.requestContentEditingInput(with: options) { [unowned self] (contentEditingInput, info) in
+                                if let urlStr = contentEditingInput?.fullSizeImageURL?.absoluteString {
+                                    self.saveResult(isSuccess: true, filePath: urlStr)
+                                }
                             }
+                        } else {
+                            self.saveResult(isSuccess: true, filePath: nil)
                         }
                     }
                 } else {
-                    self.saveResult(isSuccess: false, error: self.errorMessage)
+                    self.saveResult(isSuccess: false, error: error)
                 }
             }
         })
     }
-    
-    /// finish saving，if has error，parameters error will not nill
-    @objc func didFinishSavingImage(image: UIImage, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
-        saveResult(isSuccess: error == nil, error: error?.description)
-    }
-    
-    @objc func didFinishSavingVideo(videoPath: String, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
-        saveResult(isSuccess: error == nil, error: error?.description)
-    }
-    
-    func saveResult(isSuccess: Bool, error: String? = nil, filePath: String? = nil) {
+        
+    func saveResult(isSuccess: Bool, error: Error? = nil, filePath: String? = nil) {
         var saveResult = SaveResultModel()
         saveResult.isSuccess = error == nil
-        saveResult.errorMessage = error?.description
+        saveResult.errorMessage = error?.localizedDescription
         saveResult.filePath = filePath
         result?(saveResult.toDic())
     }
--- lib/image_gallery_saver.dart
+++ lib/image_gallery_saver.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
 class ImageGallerySaver {
-  static const MethodChannel _channel =
-      const MethodChannel('image_gallery_saver');
+  static const MethodChannel _channel = const MethodChannel('image_gallery_saver');
 
   /// save image to Gallery
   /// imageBytes can't null
@@ -14,24 +14,32 @@ class ImageGallerySaver {
   static FutureOr<dynamic> saveImage(Uint8List imageBytes,
       {int quality = 80,
       String? name,
-      bool isReturnImagePathOfIOS = false}) async {
-    final result =
-        await _channel.invokeMethod('saveImageToGallery', <String, dynamic>{
+      bool isReturnImagePathOfIOS = false,
+      double? latitude,
+      double? longitude,
+      Int? createDate}) async {
+    final result = await _channel.invokeMethod('saveImageToGallery', <String, dynamic>{
       'imageBytes': imageBytes,
       'quality': quality,
       'name': name,
-      'isReturnImagePathOfIOS': isReturnImagePathOfIOS
+      'isReturnImagePathOfIOS': isReturnImagePathOfIOS,
+      'latitude': latitude,
+      'longitude': longitude,
+      'createDate': createDate,
     });
     return result;
   }
 
   /// Save the PNG，JPG，JPEG image or video located at [file] to the local device media gallery.
-  static Future saveFile(String file, {String? name, bool isReturnPathOfIOS = false}) async {
-    final result = await _channel.invokeMethod(
-        'saveFileToGallery', <String, dynamic>{
+  static Future saveFile(String file,
+      {String? name, bool isReturnPathOfIOS = false, double? latitude, double? longitude, Int? createDate}) async {
+    final result = await _channel.invokeMethod('saveFileToGallery', <String, dynamic>{
       'file': file,
       'name': name,
-      'isReturnPathOfIOS': isReturnPathOfIOS
+      'isReturnPathOfIOS': isReturnPathOfIOS,
+      'latitude': latitude,
+      'longitude': longitude,
+      'createDate': createDate,
     });
     return result;
   }
